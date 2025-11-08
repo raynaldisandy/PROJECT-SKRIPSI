3f584638be3869f13c4fefe85ff4b894c5e454a8
Fadilah Umar <fadilahumar2000@gmail.com>
2025-11-08 14:42:13 +0700

    new update home

--- lib/Controlers/authorize_controller.dart
+++ lib/Controlers/authorize_controller.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
@@ -8,23 +6,16 @@ class AuthorizeController extends GetxController
     with GetSingleTickerProviderStateMixin {
   final PageController pageController = PageController();
 
-  late AnimationController animationController;
   Rx<String?> versionApp = Rx<String>('');
   Rx<String?> appName = Rx<String>('');
   // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   RxInt appYear = DateTime.now().year.obs;
   final RxString termsOfUse = RxString('');
   final RxString privacyPolicy = RxString('');
-  RxString tagHero = RxString('auth');
 
   @override
   void onInit() {
     super.onInit();
-    animationController = AnimationController(
-      duration: const Duration(milliseconds: 5000),
-      vsync: this,
-    );
-    animationController.repeat();
     _getValidateUser();
   }
 
